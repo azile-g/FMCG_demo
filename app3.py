c5db4d96f1142c3a81911a633460d2dce259e078
azile-g <70891408+azile-g@users.noreply.github.com>
2023-05-30 12:34:34 +0800

    update exp_rerun

--- app3.py
+++ app3.py
@@ -129,7 +129,7 @@ if format_name == format_list[0]:
 
     def restart(): 
         st.session_state.page = 0
-        st.experimental_rerun()
+        #st.experimental_rerun()
 
     placeholder = st.empty()
     st.button("Next",on_click=nextpage,disabled=(st.session_state.page > 3))
@@ -242,7 +242,9 @@ if format_name == format_list[0]:
             contents = f.read()
         st.text_input("Label of the object:", f"{contents}")
         st.image(r"writer\Img.png")
-        st.button("Restart the video capture?", on_click = restart)
+        rerun_butt = st.button("Restart the video capture?", on_click = restart)
+        if rerun_butt: 
+            st.experimental_rerun()
     
 if format_name == format_list[1]: 
     st.file_uploader("Upload Your Photos Here:")
